8e4c296fae6dd8603ced19aaf076b1f2f9d3115e
gmolarinb <guilhermedev@noisbet.com>
2024-09-03 16:22:58 -0300

    init

---
+++ .prettierrc.js
@@ -0,0 +1,7 @@
+module.exports = {
+    semi: true, //ponto e vígula
+    trailingComma: 'all', //vírgula final em objetos
+    singleQuote: true, //habilita aspas simples
+    printWidth: 80, //largura máxima da linha
+    tabWidth: 2 //2 espaços para o tab
+}
